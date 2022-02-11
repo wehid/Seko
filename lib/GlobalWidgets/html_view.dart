e35218783bcab648bc22ea5c4856f82dd072007b
Wehid <wehid.behrami@gmail.com>
2022-02-11 16:33:37 +0330

    handle clicking the link complete and showing the pdf online and fix not complete url

--- lib/GlobalWidgets/html_view.dart
+++ lib/GlobalWidgets/html_view.dart
@@ -2,15 +2,38 @@ import 'package:flutter/material.dart';
 import 'package:flutter_html/flutter_html.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'youtube_video_item.dart';
+import '../PdfView/pdf_view.dart';
 
 class HtmlView extends StatelessWidget {
   final String htmlCode;
 
   HtmlView(this.htmlCode);
 
+  String _getFileExtension(String fileName) {
+    return fileName.split('.').last;
+  }
+
+  bool _isUrlIsPdf(String urlLink) {
+    bool isPdf = _getFileExtension(urlLink) == "pdf";
+    print(isPdf);
+    return isPdf;
+  }
+
+  bool _isUrlIsComplete(String url) {
+    url.toLowerCase();
+    return url.startsWith("http");
+  }
+
+  String _completeTheUrl(String url) {
+    if (!_isUrlIsComplete(url)) {
+      url = "https://szm.one${url}";
+    }
+
+    return url;
+  }
+
   @override
   Widget build(BuildContext context) {
-
     return Html(
       data: htmlCode,
       customRender: {
@@ -22,7 +45,16 @@ class HtmlView extends StatelessWidget {
       },
       onLinkTap: (url, _, __, ___) {
         print(url);
-        launch(url);
+        _isUrlIsPdf(url)
+            ? Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => PdfView(
+                    _completeTheUrl(url),
+                  ),
+                ),
+              )
+            : launch(url);
       },
     );
   }
---
+++ lib/PdfView/pdf_view.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:advance_pdf_viewer/advance_pdf_viewer.dart';
+
+import '../constants.dart';
+
+class PdfView extends StatefulWidget {
+  final String pdfUrl;
+
+  PdfView(this.pdfUrl);
+
+  @override
+  _PdfViewState createState() => _PdfViewState();
+}
+
+class _PdfViewState extends State<PdfView> {
+  PDFDocument _pdf;
+  bool _isLoading;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadPdf();
+  }
+
+  void _loadPdf() async {
+    setState(() {
+      _isLoading = true;
+    });
+    final PDFDocument pdf = await PDFDocument.fromURL(widget.pdfUrl);
+    setState(() {
+      _pdf = pdf;
+      _isLoading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: customAppBar(widget.pdfUrl.split("/").last),
+      body: _isLoading
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : PDFViewer(
+              document: _pdf,
+              showIndicator: true,
+              lazyLoad: false,
+            ),
+    );
+  }
+}
